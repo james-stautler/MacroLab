eca4d69a440b99038ee1cdb6a8f955ea8bcc0518
James <jstautler10@gmail.com>
2026-01-07 17:05:33 -0600

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+extern "C" {
+    #include <gphoto2/gphoto2-camera.h>
+    #include <gphoto2/gphoto2-context.h>
+}
+
+#include <opencv4/opencv2/opencv.hpp>
+
+int main(int argc, char* argv[])
+{
+    printf("Everything is working!\n");
+
+    return 0;
+}
+
+
